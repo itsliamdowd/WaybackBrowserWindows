bbd72585610056425524b83cb5250d8c261997b2
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-06-05 16:02:13 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,14 +6,9 @@ app = Flask(__name__)
 @app.route("/loadpage/")
 def loadpage():
     url = request.args.get('url')
-    print(url)
     year = request.args.get('year')
-    print(year)
     month = request.args.get('month')
-    print(month)
     day = request.args.get('day')
-    print(day)
-    #url = "https://web.archive.org/web/" + str(year) + "07" + "11" + "043326if_/" + url
     url = "https://web.archive.org/web/" + str(year) + str(month) + str(day) + "043326if_/" + url
     return render_template("loadpage.html", url=url)
     
