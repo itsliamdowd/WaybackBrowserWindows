3ee808b1f5012508c93b6efe3cdcb45edff06648
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-06-06 10:15:04 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import Executable, setup
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [Executable("waybackbrowser.py", base=base)]
+
+setup(
+    name="Wayback Browser",
+    version="1.0",
+    description="Wayback Browser",
+    executables=executables,
+    includes = ['jinja2.ext'],
+    options={"build_exe":{"packages":[],
+                      "include_files":['templates\loadpage.html', 'home.png', 'logo.png', 'reload.png', 'back.png', 'forward.png', 'templates', 'templates\home.html', 'server.py']}},
+    #includefiles = ['templates\loadpage.html', 'home.png', 'logo.png', 'reload.png', 'back.png', 'forward.png', 'templates', 'home.html', 'server.py'],
+)
